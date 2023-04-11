1cbfde41f041c44071eef4896c481927762b1e1d
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-04-11 14:13:30 +0900

    [level 1] Title: 소수 만들기, Time: 2.21 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,35 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = -1;
-        int numLen = nums.length;
 
-        List<Integer> sumList = new ArrayList<>();
-        for(int i=0; i< numLen-2; i++){
-            for(int j=i+1; j<numLen-1; j++){
-                for(int k=j+1; k<numLen; k++) {
-                    sumList.add(nums[i]+nums[j]+nums[k]);
-                }
-            }
-        }
-
-        System.out.println("sumList = " + sumList);
-        //sumList에 있는 숫자들을 소수인지 아닌지 판별하여 카운트
-        int count = 0;
-        for (Integer integer : sumList) {
-            int numcount = 0;
-            for(int i = 2; i<integer;i++){
-                if(integer%i == 0) {
-                    numcount++;
-                }
-            }
-            if(numcount == 0) count++;
-        }
-
-        answer = count;
-
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return answer;
     }
