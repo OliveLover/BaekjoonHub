ed3cf7f087c3bfdd821d7f92d779a66589d0e8c9
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-06-05 02:45:11 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a + b);
+
+        System.out.println(sb);
+
+    }
+}
