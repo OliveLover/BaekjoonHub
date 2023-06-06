d6506a375fa2450a86990ecdf77217b2016d8c50
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-06-06 14:10:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String star = "*";
+    int num = Integer.parseInt(br.readLine());
+    for(int i=0; i<num; i++) {
+      for(int j=0; j<=i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+}
