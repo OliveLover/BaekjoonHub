72a1fe94ac5f29b8d9d1130b759548a2d9e26afa
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-06-25 17:46:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,35 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        ArrayList<Boolean> primeList;
+class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        int[] numList = new int[n];
-        for(int i = 0; i < n; i++) {
-            numList[i] = scanner.nextInt();
-        }
+    int n = Integer.parseInt(br.readLine());
 
-        primeList = new ArrayList<Boolean>(1001);
+    int[] arr = new int[n];
 
-        primeList.add(false);
-        primeList.add(false);
-
-        for(int i=2; i<=1000; i++) primeList.add(i,true);
-
-        for (int i = 2; (i * i) <= 1000; i++) {
-            if (primeList.get(i)) {
-                for (int j = i * i; j <= 1000; j += i) primeList.set(j, false);
-            }
-        }
-        int count = 0;
-        for (int i : numList) {
-            if (primeList.get(i)) {
-                count++;
-            }
-        }
-        System.out.println(count);
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    
+    for(int i=0; i<n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    ArrayList<Boolean> primeList = new ArrayList<>(1001);
+
+    primeList.add(false);
+    primeList.add(false);
+
+    for(int i=2; i<=1001; i++) {
+      primeList.add(i, true);
+    }
+
+    for(int i=2; i*i<=1001; i++) {
+      if(primeList.get(i)) {
+        for(int j=i*i; j<=1001; j+= i) primeList.set(j, false);
+      }
+    }
+
+    int count = 0;
+    
+    for(int i=0; i<n; i++) {
+      if(primeList.get(arr[i])) count++;
+    }
+
+    System.out.println(count);
+    
+  }
 }
