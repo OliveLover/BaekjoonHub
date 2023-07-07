99bd33ca528d7e1c59fc8e188b869420127e7b0e
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-07-07 14:22:13 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        double v = Double.parseDouble(st.nextToken());
 
-        double a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int v = scanner.nextInt();
-        int day;
+        int answer = 0;
 
-        System.out.println((int)Math.ceil((v-b)/(a-b)));
+        answer = (int) Math.ceil((v - b) / (a - b));
+        System.out.println(answer);
     }
+
 }
