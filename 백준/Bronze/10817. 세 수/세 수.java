0fe2d4a50a3b03825cc48ac3366e388314d5f3c8
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-07-26 10:24:15 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,33 +1,71 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {  
+public class Main {
     public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-      int[] arr = br.readLine()
-        .lines()
-        .flatMap(line -> Arrays.stream(line.split(" ")))
-                 .limit(3)
-                 .mapToInt(Integer::parseInt)
-                 .toArray();
+        int[] arr = new int[3];
+        for(int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-      bubbleSort(arr);
-      System.out.println(arr[1]);
+        quickSort(arr, 0, 2);
+
+        System.out.println(arr[1]);
     }
-  public static void bubbleSort(int[] arr) {
-    for(int i = 0; i < 2; i++) {
-      for(int j = 0; j < 2 - i; j++) {
-        if(arr[j] > arr[j+1]) swap(arr, j, j + 1);
-      }
-    }
-  }
 
-  public static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+    public static void quickSort(int[] arr, int s, int e) {
+        if(s < e) {
+            int pivot = partition(arr, s, e);
+
+            quickSort(arr, s, pivot - 1);
+            quickSort(arr, pivot + 1, e);
+        }
+    }
+
+    public static int partition(int[] arr, int s, int e) {
+        if(s + 1 == e) {
+            if(arr[s] > arr[e]) {
+                swap(arr, s, e);
+            }
+            return e;
+        }
+
+        int m = s + e / 2;
+        swap(arr, s, m);
+        int pivot = arr[s];
+
+        int i = s + 1;
+        int j = e;
+
+        while(i <= j) {
+            while(arr[j] > pivot && j > 0) {
+                j--;
+            }
+            while(arr[i] < pivot && i < 2) {
+                i++;
+            }
+            if(i < j) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            } else {
+                break;
+            }
+        }
+        arr[s] = arr[j];
+        arr[j] = pivot;
+
+        return j;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
