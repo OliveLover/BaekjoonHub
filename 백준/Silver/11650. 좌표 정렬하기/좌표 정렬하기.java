af44e711b0d31239d9bfa4ea0e984be69fa191bc
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-08-03 21:28:39 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1044 ms, Memory: 73228 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> numList = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            numList.add(new ArrayList<>());
+            st = new StringTokenizer(br.readLine(), " ");
+
+            numList.get(i).add(Integer.parseInt(st.nextToken()));
+            numList.get(i).add(Integer.parseInt(st.nextToken()));
+        }
+
+        numList.sort(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                if(Objects.equals(o1.get(0), o2.get(0))) {
+                    return o1.get(1).compareTo(o2.get(1));
+                }
+                return o1.get(0).compareTo(o2.get(0));
+            }
+        });
+
+        for(int i = 0; i < n; i++) {
+            sb.append(numList.get(i).get(0)).append(" ");
+            sb.append(numList.get(i).get(1)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
