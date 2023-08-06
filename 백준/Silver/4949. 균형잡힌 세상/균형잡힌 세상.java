b713a056d3f8bc0d11c044d5efc92d1956980323
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-08-06 11:03:01 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 248 ms, Memory: 19600 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> bracketStack;
+
+        String sentence = "";
+        while (!Objects.equals(sentence = br.readLine(), ".")) {
+            bracketStack = new Stack<>();
+
+            int sentenceLength = sentence.length();
+
+            for (int i = 0; i < sentenceLength; i++) {
+                char word = sentence.charAt(i);
+                if(!(word == '(' || word == '{' || word == '[' || word == ')' || word == '}' || word == ']')) continue;
+                
+                if(word == ')' && !bracketStack.isEmpty() && bracketStack.peek() == '(') {
+                    bracketStack.pop();
+                } else if (word == '}' && !bracketStack.isEmpty() && bracketStack.peek() == '{') {
+                    bracketStack.pop();
+                } else if (word == ']' && !bracketStack.isEmpty() && bracketStack.peek() == '[') {
+                    bracketStack.pop();
+                } else {
+                    bracketStack.push(word);
+                }
+            }
+            
+            if (bracketStack.isEmpty())
+                sb.append("yes").append("\n");
+            else sb.append("no").append("\n");
+
+
+        }
+        System.out.println(sb);
+    }
+}
