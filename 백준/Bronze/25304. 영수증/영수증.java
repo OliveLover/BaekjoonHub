ad0bdd54f0a68b82a6d63ebfa1b1ddb954877f12
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-09-13 20:02:30 +0900

    [Bronze IV] Title: 영수증, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int a, b, sum = 0;
+
+        for (int i=0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            sum += (a * b);
+        }
+
+        if (sum == X) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
