75827288619aeb0615ec35ba4f081a5c998752a5
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-09-21 17:21:51 +0900

    [Gold V] Title: 신기한 소수, Time: 484 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,42 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-  static int n;
-  static List<Integer> primeList = new ArrayList<>();
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    
-    n = Integer.parseInt(br.readLine());
+    static List<Integer> primeList = new ArrayList<>();
+    static int n;
 
-    DFS(2);
-    DFS(3);
-    DFS(5);
-    DFS(7);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-    for (int i : primeList) sb.append(i).append("\n");
+        n = Integer.parseInt(br.readLine());
 
-    System.out.print(sb);
-  }
+        DFS(2);
+        DFS(3);
+        DFS(5);
+        DFS(7);
 
-  private static void DFS(int v) {
-    if (n == String.valueOf(v).length()) primeList.add(v);
-    else {
-      for (int i = 1; i < 10; i +=2) {
-        if (isPrime(v*10 + i)) DFS(v*10 + i);
-      }
+        for (int i : primeList) sb.append(i).append("\n");
+
+        System.out.println(sb);
     }
-  }
 
-  private static boolean isPrime(int num) {
-    for (int i = 2; i < (num / 2 + 1); i++) {
-      if (num % i == 0) return false;
+    static void DFS(int number) {
+        if (n == String.valueOf(number).length()) {
+            primeList.add(number);
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i);
+            }
+        }
     }
-    return true;
-  }
-  
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
