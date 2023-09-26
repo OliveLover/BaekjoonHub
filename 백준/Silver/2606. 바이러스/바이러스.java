a65b44f7a3ea45996aa94476041b3df729430228
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-09-26 13:07:00 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        DFS(1);
+
+        System.out.println(count - 1);
+    }
+
+    private static void DFS(int startNumber) {
+        if (visited[startNumber]) return;
+
+        visited[startNumber] = true;
+        count++;
+
+        for (int i : A[startNumber]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
