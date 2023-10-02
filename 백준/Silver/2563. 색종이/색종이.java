de621691853fa6a664fb3f5880cb6919556e35d9
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-10-02 13:15:36 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] map = new int[101][101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (map[j][k] == 0) {
+                        map[j][k] = 1;
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
