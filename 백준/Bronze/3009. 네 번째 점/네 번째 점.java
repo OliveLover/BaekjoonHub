1bd191f5245fcde8a7d02bbfd74d9ef7039927c7
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-10-14 14:13:35 +0900

    [Bronze III] Title: 네 번째 점, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] dx = new int[3];
+        int[] dy = new int[3];
+
+        int countMinX = 0;
+        int countMaxX = 0;
+        int countMinY = 0;
+        int countMaxY = 0;
+
+
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (minX > x) minX = x;
+            if (maxX < x) maxX = x;
+            if (minY > y) minY = y;
+            if (maxY < y) maxY = y;
+
+            dx[i] = x;
+            dy[i] = y;
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (minX == dx[i]) countMinX++;
+            if (maxX == dx[i]) countMaxX++;
+            if (minY == dy[i]) countMinY++;
+            if (maxY == dy[i]) countMaxY++;
+        }
+
+        if (countMaxX == 1) {
+            sb.append(maxX).append(" ");
+        } else {
+            sb.append(minX).append(" ");
+        }
+
+        if (countMaxY == 1) {
+            sb.append(maxY);
+        } else {
+            sb.append(minY);
+        }
+
+        System.out.println(sb);
+    }
+}
