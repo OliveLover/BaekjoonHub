c9b27d30c14aafd7a57297e9eb14e5d570f3dcb0
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-11-21 23:39:55 +0900

    [Bronze V] Title: A/B, Time: 148 ms, Memory: 9744 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  const input = line.split(' ');
+
+  const a = parseFloat(input[0]);
+  const b = Number(input[1]);
+
+  const result = a / b;
+
+  console.log(result);
+
+  rl.close();
+})
