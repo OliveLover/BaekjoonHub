2e68d3c582e10649486b2c06ced6dec83d0a21b6
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-11-23 19:49:04 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String w = br.readLine();
 
-        String str = in.nextLine();
+    sb.append(w).append("??!");
 
-        System.out.println(str + "??!");
-    }
+    System.out.println(sb);
+  }
 }
