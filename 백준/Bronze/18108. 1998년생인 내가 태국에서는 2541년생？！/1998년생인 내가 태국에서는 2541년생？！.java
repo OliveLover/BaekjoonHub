53039b422e97213783cbc2f3f2eb1f31f5d2cd18
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2023-12-03 13:43:42 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = in.nextInt() - 543;
-        System.out.println(num);
-    }
+    int y = Integer.parseInt(br.readLine());
+    int result = y - 543;
+    System.out.println(result);
+  }
 }
