6f7078ae29a5fef22d3897f50116db1926fcb75f
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2024-08-31 00:37:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 132 ms, Memory: 15312 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,22 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
             int a = Integer.parseInt(st.nextToken());
-            if(x > a) sb.append(a).append(" ");
+            int x = Integer.parseInt(st.nextToken());
+
+            int[] arr = new int[a];
+            int arrSize = arr.length;
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < arrSize; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                if (arr[i] < x) {
+                    sb.append(arr[i]).append(" ");
+                }
+            }
+            System.out.println(sb);
         }
-        System.out.println(sb);
-    }
 }
