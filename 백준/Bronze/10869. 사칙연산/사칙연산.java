d89062b0e0e64ee0787f019fceec1e3a2f0f5447
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2024-09-01 23:51:15 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -6,17 +6,18 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
         sb.append(a + b).append("\n")
-                .append(a - b).append("\n")
-                .append(a * b).append("\n")
-                .append(a / b).append("\n")
-                .append(a % b);
+          .append(a - b).append("\n")
+          .append(a * b).append("\n")
+          .append(a / b).append("\n")
+          .append(a % b);
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
