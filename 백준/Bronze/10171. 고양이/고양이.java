1f727a54d3ccd7c182665b4b72910a4064e105b4
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2024-09-01 22:36:10 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13848 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+
+        String cat = "\\    /\\" + "\n" +
+                " )  ( ')" + "\n" +
+                "(  /  )" + "\n" +
+                " \\(__)|";
+
+        System.out.println(cat);
     }
 }
