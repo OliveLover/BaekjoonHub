23bfc0897d51d2703e6185801e22ec0d7c0b6bb5
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2024-09-02 00:10:37 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -5,17 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int score = Integer.parseInt(br.readLine());
 
-        if (score >= 90)
-            System.out.println('A');
-        else if (score >= 80)
-            System.out.println('B');
-        else if (score >= 70)
-            System.out.println('C');
-        else if (score >= 60)
-            System.out.println('D');
-        else
-            System.out.println('F');
+        int score = Integer.parseInt(br.readLine());
+        char result;
+
+        if (score >= 90) {
+            result = 'A';
+        } else if (score >= 80) {
+            result = 'B';
+        } else if (score >= 70) {
+            result = 'C';
+        } else if (score >= 60) {
+            result = 'D';
+        } else {
+            result= 'F';
+        }
+
+        System.out.println(result);
     }
 }
