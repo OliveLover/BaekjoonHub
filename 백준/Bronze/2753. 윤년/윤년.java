f3e75e81ee9c5c8ce48f770aa8660fba85f793c8
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2024-09-02 13:42:13 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -7,11 +7,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int year = Integer.parseInt(br.readLine());
-        int result = 0;
-        if(year % 4 == 0) {
-            if(!(year % 100 == 0)) result = 1;
-            if(year % 400 == 0) result = 1;
+        int answer = 0;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            answer = 1;
         }
-        System.out.println(result);
+
+        System.out.println(answer);
     }
 }
