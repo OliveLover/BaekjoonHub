e82104555ea2bd5a39aa117f1c3741fb8dc59370
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2025-02-17 09:51:44 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -8,9 +8,10 @@ import java.util.StringTokenizer;
 public class Main {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
     static int[][] maze;
+    static boolean[][] visited;
     static int n, m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,24 +30,27 @@ public class Main {
         }
 
         BFS(0, 0);
+
         System.out.println(maze[n - 1][m - 1]);
     }
 
-    public static void BFS(int i, int j) {
+    private static void BFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j});
+        queue.offer(new int[]{i, j});
         visited[i][j] = true;
 
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
+
             for (int k = 0; k < 4; k++) {
                 int x = now[0] + dx[k];
                 int y = now[1] + dy[k];
-                if (x >= 0 && y >= 0 && x < n && y < m) {
+
+                if (x >= 0 && y >= 0 && n > x && m > y) {
                     if (maze[x][y] != 0 && !visited[x][y]) {
                         visited[x][y] = true;
                         maze[x][y] = maze[now[0]][now[1]] + 1;
-                        queue.add(new int[] {x, y});
+                        queue.offer(new int[] {x, y});
                     }
                 }
             }
