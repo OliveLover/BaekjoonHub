6d467f0e4074433a1b6d6f465fb127465e6a72e3
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2025-02-18 16:51:39 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,29 +1,6 @@
-SELECT   h.HOUR
-       , COUNT(h.HOUR) AS COUNT
-  FROM   ANIMAL_OUTS a
-        , (
-                SELECT 9 AS HOUR
-                UNION ALL
-                SELECT 10
-                UNION ALL
-                SELECT 11
-                UNION ALL
-                SELECT 12
-                UNION ALL
-                SELECT 13
-                UNION ALL
-                SELECT 14
-                UNION ALL
-                SELECT 15
-                UNION ALL
-                SELECT 16
-                UNION ALL
-                SELECT 17
-                UNION ALL
-                SELECT 18
-                UNION ALl
-                SELECT 19
-          ) AS h
- WHERE HOUR(a.DATETIME) = h.HOUR
- GROUP BY h.HOUR
- ORDER BY h.HOUR;
+SELECT   HOUR(DATETIME) AS HOUR
+       , COUNT(*) AS COUNT
+  FROM ANIMAL_OUTS
+ WHERE HOUR(DATETIME) BETWEEN 9 AND 19 
+ GROUP BY HOUR(DATETIME)
+ ORDER BY HOUR;
