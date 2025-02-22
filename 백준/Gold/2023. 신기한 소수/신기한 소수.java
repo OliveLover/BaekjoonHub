fd0c89315bea08756dfda87a714e7b148cc9c7a9
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2025-02-22 16:55:02 +0900

    [Gold V] Title: 신기한 소수, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,45 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
   static int n;
-  static ArrayList<Integer> primeList = new ArrayList<>();
+  static StringBuilder sb;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
+    sb = new StringBuilder();
     n = Integer.parseInt(br.readLine());
 
-    DFS(2);
-    DFS(3);
-    DFS(5);
-    DFS(7);
+    dfs(2);
+    dfs(3);
+    dfs(5);
+    dfs(7);
 
-    primeList.stream().forEach(num -> sb.append(num).append("\n"));
     System.out.print(sb);
   }
 
-  static void DFS(int number) {
-    if (n == String.valueOf(number).length()) {
-      primeList.add(number);
+  static void dfs(int number) {
+    if (String.valueOf(number).length() == n) {
+      sb.append(number).append("\n");
       return;
     }
 
     for (int i = 1; i < 10; i++) {
       if (i % 2 != 0) {
-        if (isPrime(number * 10 + i)) {
-          DFS(number * 10 + i);
+        if (isPrimeNumber(number * 10 + i)) {
+          dfs(number * 10 + i);
         }
       }
     }
   }
 
-  static boolean isPrime(int number) {
-    for (int i = 2; i <= (int)Math.sqrt(number); i++) {
-      if (number % i == 0)
-        return false;
+  static boolean isPrimeNumber(int number) {
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0) return false;
     }
     return true;
   }
