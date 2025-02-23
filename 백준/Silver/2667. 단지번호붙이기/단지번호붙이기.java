40b7c09aa2c47ebd28d22da39af42bea12ccb25f
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2025-02-23 10:31:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,67 +4,73 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] residential;
-    static boolean[][] visited;
+    static int[] DX = {1, 0, -1, 0};
+    static int[] DY = {0, 1, 0, -1};
+    static int[][] A;
+    static boolean[][] VISITED;
     static int n;
-    static int count = 0;
-    static int house = 0;
+    static int sectionCount = 0;
+    static int houseCount = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
 
-        residential = new int[n][n];
-        visited = new boolean[n][n];
-        List<Integer> record = new ArrayList<>(n * n / 2);
+        A = new int[n][n];
+        VISITED = new boolean[n][n];
+        ArrayList<Integer> records = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            String numStr = br.readLine();
+            st = new StringTokenizer(br.readLine());
+
+            String line = st.nextToken();
             for (int j = 0; j < n; j++) {
-                residential[i][j] = numStr.charAt(j) - '0';
+                A[i][j] = Integer.parseInt(line.substring(j, j + 1));
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && residential[i][j] != 0) {
-                    house = 0;
+                if (!VISITED[i][j] && A[i][j] != 0) {
+                    houseCount = 0;
                     BFS(i, j);
-                    record.add(house);
+                    records.add(houseCount);
                 }
             }
         }
-        Collections.sort(record);
 
-        sb.append(count).append("\n");
-        for (int i : record) {
+        Collections.sort(records);
+
+        sb.append(sectionCount).append("\n");
+        for (int i : records) {
             sb.append(i).append("\n");
         }
 
         System.out.print(sb);
+        br.close();
     }
 
-    private static void BFS(int i, int j) {
-        count++;
-        house++;
+    static void BFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j});
-        visited[i][j] = true;
+        queue.add(new int[]{i, j});
+        VISITED[i][j] = true;
+        sectionCount++;
+        houseCount++;
 
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
 
             for (int k = 0; k < 4; k++) {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
+                int x = now[0] + DX[k];
+                int y = now[1] + DY[k];
 
-                if (x >= 0 && y >= 0 && n > x && n > y) {
-                    if(residential[x][y] != 0 && !visited[x][y]) {
-                        visited[x][y] = true;
-                        house++;
-                        queue.offer(new int[] {x, y});
+                if (x >= 0 && y >= 0 && x < n && y < n) {
+                    if (!VISITED[x][y] && A[x][y] != 0) {
+                        VISITED[x][y] = true;
+                        houseCount++;
+                        queue.add(new int[]{x, y});
                     }
                 }
             }
