1f0ca274897446f180dd18241146895bc879dba2
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2025-02-25 19:01:25 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,15 +1,13 @@
-SELECT   u.BOARD_ID
-       , u.WRITER_ID
-       , u.TITLE
-       , u.PRICE
-       , (CASE
-               WHEN u.STATUS = 'SALE'
-               THEN '판매중'
-               WHEN u.STATUS = 'RESERVED'
-               THEN '예약중'
-               ELSE '거래완료'
-          END
-         ) AS STATUS
-  FROM USED_GOODS_BOARD u
- WHERE DATE_FORMAT(u.CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
- ORDER BY u.BOARD_ID DESC;
+SELECT   BOARD_ID
+       , WRITER_ID
+       , TITLE
+       , PRICE
+       , (CASE 
+              WHEN STATUS = 'SALE' THEN '판매중'
+              WHEN STATUS = 'RESERVED' THEN '예약중'
+              ELSE '거래완료'
+              END
+          ) AS STATUS
+  FROM USED_GOODS_BOARD
+ WHERE CREATED_DATE = '2022-10-05'
+ ORDER BY BOARD_ID DESC;
