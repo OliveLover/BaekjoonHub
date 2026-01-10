f80f1b2f2de4889a4e044d9067c87c29d79c587e
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2026-01-10 19:17:09 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.c
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int a, b;
+    int a, b = 0;
 
-  scanf("%d %d", &a, &b);
-
-  printf("%d", a - b);
-
-  return 0;
+    scanf("%d %d", &a, &b);
+    printf("%d", a - b);
 }
