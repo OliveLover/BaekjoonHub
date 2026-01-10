4f18acd6269917f72890f4c0c558024d5daa81ce
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2026-01-10 17:59:37 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.c
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int a, b;
+    int a, b;
 
-  scanf("%d %d", &a, &b);
-
-  printf("%d\n", a + b);
-  
-  return 0;
+    scanf("%d %d", &a, &b);
+    
+    printf("%d\n", a + b);
 }
