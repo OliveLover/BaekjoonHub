250a1a2d830c18c34ed2bd8c4a2768c0e21eb651
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2026-01-11 15:32:38 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.c
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-  int a, b;
+int main() {
+    int a = 0, b = 0;
 
-  scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b);
+    printf("%d\n%d\n%d\n%d\n%d\n", a + b, a - b, a * b, a / b, a % b);
 
-  printf("%d\n", a + b);
-  printf("%d\n", a - b);
-  printf("%d\n", a * b);
-  printf("%d\n", a / b);
-  printf("%d", a % b);
+    return 0;
 }
