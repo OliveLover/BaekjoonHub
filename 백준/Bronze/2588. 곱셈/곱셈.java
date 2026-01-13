1929acdbbcbf965fa6d502373d9d38a009a73ed5
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2026-01-13 22:33:06 +0900

    [Bronze III] Title: 곱셈, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,20 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    int a = Integer.parseInt(br.readLine());
-    String b = br.readLine();
-    
-    for (int i = 2; i >= 0; i--) {
-      int w = b.charAt(i) - '0';
-      sb.append(a * w).append("\n");
-    }
-    sb.append(a * Integer.parseInt(b));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-    System.out.println(sb);
-  }
+        int[] numbers = new int[2];
+        
+        for(int i = 0; i < 2; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        String strNumber = String.valueOf(numbers[1]);
+        int[] charNumbers = new int[3];
+        for(int i = 0; i < 3; i++) {
+            charNumbers[i] = strNumber.charAt(i) - '0';
+        }
+
+        int result = 0;
+        int multiplier = 1;
+        for(int i = 2; i >= 0; i--) {
+            int multi = numbers[0] * charNumbers[i];
+            sb.append(multi).append("\n");
+            result += multi * multiplier;
+            multiplier *= 10;
+        }
+
+        sb.append(result);
+
+        System.out.println(sb);
+
+        br.close();
+    }
 }
