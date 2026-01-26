66daee3cdca043f8be86292075bc10d896670d87
LeeHyeonGyu <118647313+OliveLover@users.noreply.github.com>
2026-01-26 21:47:14 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,22 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int X = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
-        int a, b, sum = 0;
 
-        for (int i=0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int a = 0, b = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
-            sum += (a * b);
+
+            x -= a * b;
         }
 
-        if (sum == X) System.out.println("Yes");
-        else System.out.println("No");
+        System.out.println(x == 0 ? "Yes" : "No");
+        
+        br.close();
     }
 }
